3683f1ad1e8d3a81b7d4802842d2c46609940d11
[zemenuyar] <[zemenuyar@gmail.com]>
2018-10-04 10:27:49 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x , y, z;
+        x = 0;
+        if (x > 5) {
+            y = 3;
+            System.out.println(y);
+            z = 4;
+            System.out.println(z);
+        } else {
+            y = 4;
+            System.out.println(y);
+            z = 5;
+            System.out.println(z);
+        }
+    }
+}
